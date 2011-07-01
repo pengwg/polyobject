28da9ddeeaab4431f3b88c19d6161bad64041c9e
Peng Wang <pengwg@gmail.com>
2011-07-01 18:09:34 -0500

    Progress

---
+++ polyObject.cpp
@@ -0,0 +1,10 @@
+#include <QXmlStreamWriter>
+#include <vtkPolyDataReader.h>
+
+int main(int argc, char *argv[])
+{
+    QString fileName;
+    vtkPolyDataReader *reader = vtkPolyDataReader::New();
+    return 0;
+}
+
